8f83bbe94a0c511eeb65b31e93d096f4b81696cb
JoggerJoel <joel.labelle@gmail.com>
2021-12-28 10:19:30 -0500

    Update main.go

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// access the API to list pods
 	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
 
-	w.Write([]byte("There are ")
+	w.Write([]byte("There are "))
 	w.Write([]byte(len(pods.Items))
 	w.Write([]byte("in the cluster"))
 	w.Write([]byte(`"}`))
