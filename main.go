631b37237c34288e1d6698f9a34202baa482ce56
JoggerJoel <joel.labelle@gmail.com>
2021-12-28 07:08:13 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Server struct{}
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	
+	ifaces, err := net.Interfaces()
+	// handle err
+	for _, i := range ifaces {
+	    addrs, err := i.Addrs()
+	    // handle err
+	    for _, addr := range addrs {
+		var ip net.IP
+		switch v := addr.(type) {
+		case *net.IPNet:
+			ip = v.IP
+		case *net.IPAddr:
+			ip = v.IP
+		}
+		// process IP address
+	    }
+	}
+
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "Hello World!"}`))
+	w.Write([]byte(`{"message": "Hello World: ", ip}`))
+	
+	
 }
 
 func main() {
