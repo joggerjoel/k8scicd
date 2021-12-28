0a6bb299f6c4b77709470384551b9afc6cded58c
JoggerJoel <joel.labelle@gmail.com>
2021-12-28 10:44:31 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import (
   "os"
   "log"
   "path/filepath"	
-  "context"
-  "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/client-go/kubernetes"
-  "k8s.io/client-go/tools/clientcmd"
+
+	
+	"context"
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
   "net/http"
   "net"
   "strconv"
@@ -41,21 +56,43 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`/`))
 	w.Write([]byte(GetLocalIP()))
 	
-	kubeconfig := filepath.Join(
-	    os.Getenv("HOME"), ".kube", "config",
-	)
-	// uses the current context in kubeconfig
-	// path-to-kubeconfig -- for example, /root/.kube/config
-	config, _ := clientcmd.BuildConfigFromFlags("", kubeconfig)
+// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
 	// creates the clientset
-	clientset, _ := kubernetes.NewForConfig(config)
-	// access the API to list pods
-	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
-
-	if (pods != nil && pods.Items != nil) {
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		// get pods in all the namespaces by omitting namespace
+		// Or specify namespace to get pods in particular namespace
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 		w.Write([]byte("There are "))
 		w.Write([]byte(strconv.Itoa(len(pods.Items))))
-		w.Write([]byte("in the cluster"))
+		w.Write([]byte("pods in the cluster"))
+
+		// Examples for error handling:
+		// - Use helper functions e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found example-xxxxx pod in default namespace\n")
+		}
+
+		time.Sleep(10 * time.Second)
 	}
 	w.Write([]byte(`"}`))
 	
