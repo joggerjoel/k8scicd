f7f799c00bf58e6dbe25de62ab19c6993e77637d
JoggerJoel <joel.labelle@gmail.com>
2021-12-28 07:31:26 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"os"
+	"os/signal"
+	"time"
 	"log"
 	"net/http"
 	"net"
@@ -25,11 +31,59 @@ func GetLocalIP() string {
     return ""
 }
 
+//
+// Pod infos
+//
+
+func GetPodDetails() () {
+
+    // creates the in-cluster config
+    config, err := rest.InClusterConfig()
+    if err != nil {
+        panic(err.Error())
+    }
+    // creates the clientset
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    IP = GetLocalIP()
+    for {
+        if IP != "" {
+            break
+        } else {
+            log.Printf("No IP for now.\n")
+        }
+
+        pods, err := clientset.CoreV1().Pods("default").List(metav1.ListOptions{})
+        if err != nil {
+            panic(err.Error())
+        }
+        for _, pod := range pods.Items {
+            pod, _ := clientset.CoreV1().Pods("default").Get(pod.Name, metav1.GetOptions{})
+            if pod.Name == os.Getenv("HOSTNAME") {
+                IP = pod.Status.PodIP
+            }
+        }
+
+        log.Printf("Waits...\n")
+        time.Sleep(1 * time.Second)
+    }
+
+    name = os.Getenv("HOSTNAME")
+    log.Printf("Trying os.Getenv(\"HOSTNAME/IP\"): [%s][%s]\n", name, IP)
+
+    return IP, name
+}
+
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"message": "Hello World: `))
+	w.Write([]byte(GetLocalIP()))
+	w.Write([]byte(`/`))
 	w.Write([]byte(GetLocalIP()))
 	w.Write([]byte(`"}`))
 	
