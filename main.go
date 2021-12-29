571d00c26322f0d430a320b6810bd8addac5ac5b
JoggerJoel <joel.labelle@gmail.com>
2021-12-29 17:56:06 -0500

    Update main.go

--- main.go
+++ main.go
@@ -33,8 +33,6 @@ const (
 )
 
 type Server struct{}
-os.Setenv("KUBERNETES_SERVICE_HOST", "192.168.1.96")
-os.Setenv("KUBERNETES_SERVICE_PORT", 6443)
 
 // GetLocalIP returns the non loopback local IP of the host
 func GetLocalIP() string {
@@ -105,6 +103,9 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {	
+	os.Setenv("KUBERNETES_SERVICE_HOST", "192.168.1.96")
+	os.Setenv("KUBERNETES_SERVICE_PORT", 6443)
+
 	s := &Server{}
 	http.Handle("/", s)
 	log.Fatal(http.ListenAndServe(":8080", nil))
