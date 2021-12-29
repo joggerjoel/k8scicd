456d96233555afe2f45fb06a1bfa53445fb0aba3
JoggerJoel <joel.labelle@gmail.com>
2021-12-29 06:59:46 -0500

    Update main.go

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	
 	
 	var kubeconfig *string
-	kubeconfig = flag.String("kubeconfig", filepath.Join("/var/lib/jenkins/.kube", "config"), "(optional) absolute path to the kubeconfig file")
+	kubeconfig = flag.String("kubeconfig", filepath.Join("/kube/.kube", "config"), "(optional) absolute path to the kubeconfig file")
 
 	flag.Parse()
 
